7b8e60dde4d8b64d553d9398c3fa9dcc5310d1ea
Himanshu-Pandey <38459237+Himanshu-Pandey@users.noreply.github.com>
2018-04-25 15:41:29 +0530

    Update raft-init.sh

--- raft-init.sh
+++ raft-init.sh
@@ -10,7 +10,7 @@ echo "[*] Configuring node"
 mkdir -p qdata/{keystore,geth}
 cp raft/static-nodes.json qdata
 
-geth --datadir qdata account new 
+geth --datadir qdata --password passwords.txt account new 
 cp raft/geth/nodekey qdata/geth/nodekey
 
 geth --datadir qdata init genesis.json
