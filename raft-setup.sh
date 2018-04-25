46d1227de70e56bebdac9a88bd777a7944328315
Himanshu-Pandey <38459237+Himanshu-Pandey@users.noreply.github.com>
2018-04-25 16:21:08 +0530

    Update raft-setup.sh

--- raft-setup.sh
+++ raft-setup.sh
@@ -9,7 +9,7 @@ mkdir raft
 echo "[*] Generating geth node config"
 nohup geth --datadir raft 2>> raft/setup.log &
 sleep 3
-echo "[\"$(cat raft/setup.log | grep -oEi '(enode.*@)'|head -1)$(hostname -I | cut -f2 -d " "):21000?discport=0&raftport=23000\"]" >> raft/static-nodes.json
+echo "[\"$(cat raft/setup.log | grep -oEi '(enode.*@)'|head -1)$(hostname -I):21000?discport=0&raftport=23000\"]" >> raft/static-nodes.json
 
 # echo "[*] Creating default ethereum account"
 # geth --datadir raft --password passwords.txt account new
