c7a0c54d7a739055330b5fee48b55b85e861acff
phiman <phiman@INphiman.local>
2018-04-26 16:33:03 +0530

    deleted old files

--- constellation-start.sh
+++
@@ -1,6 +0,0 @@
-set -u
-set -e
-
-echo "[*] Starting Constellation node"
-ARGS="--url=$(hostname -I | cut -f1 -d " ") --port=9000 --othernodes=[$1] --socket=qdata/tm.ipc  --publickeys=[raft/constellation.pub]  --privatekeys=raft/constellation.key  storage=qdata/constellation --verbosity=3"
-nohup constellation-node $ARGS  2>> qdata/logs/constellation.log &
