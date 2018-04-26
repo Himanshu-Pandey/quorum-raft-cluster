eb9cbb09cd0b5db6b43a6bee7c937d9d56872f22
Himanshu-Pandey <38459237+Himanshu-Pandey@users.noreply.github.com>
2018-04-26 16:26:22 +0530

    Update new-node.sh

--- new-node.sh
+++ new-node.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 set -u
 set -e
 
@@ -18,38 +17,39 @@ echo "[*] Generating constellation key pair"
 cd raft
 echo "" | constellation-node --generatekeys=constellation
 
+cd ..
+
 echo "[*] Cleaning up temporary data directories"
 rm -rf qdata
 mkdir -p qdata/logs
 
 echo "[*] Making new account"
-cd ..
 mkdir -p qdata/{keystore,geth}
 cp raft/static-nodes.json qdata
 
-geth --datadir qdata --password passwords.txt account new 
+geth --datadir qdata --password passwords.txt account new
 cp raft/geth/nodekey qdata/geth/nodekey
 
 echo "[*] Initialising geth"
 geth --datadir qdata init genesis.json
 
 echo "[*] Starting Constellation node"
-ARGS="--url=$(hostname -I | cut -f2 -d " ") --port=9000 --othernodes=[$1] --socket=qdata/tm.ipc  --publickeys=raft/constellation.pub --privatekeys=raft/constellation.key --verbosity=3"
+ARGS="--url=http://$(hostname -I | cut -f2 -d " "):9000/ --port=9000 --workdir=raft --othernodes=[$1:9000/] --socket=tm.ipc  --publickeys=constellation.pub --privatekeys=constellation.key --verbosity=3"
 nohup constellation-node $ARGS  2>> qdata/logs/constellation.log &
 
 if [[ $1 ]]
 then
     echo "[*] installing pip and web3py"
-    export LC_ALL="en_US.UTF-8" 
-    export LC_CTYPE="en_US.UTF-8" 
+    export LC_ALL="en_US.UTF-8"
+    export LC_CTYPE="en_US.UTF-8"
     sudo apt-get install python3-pip -y
     pip3 install web3
 
     echo "[*] calling admin.add peer on http://$1:22000"
     ENODE_ADDR=$(cat raft/static-nodes.json | python -c "import json,sys;obj=json.load(sys.stdin);print obj[0];")
-    python3 add-peer.py "http://$1:22000" $ENODE_ADDR
+    python3 add-peer.py "$1:22000/" $ENODE_ADDR
 fi
 
 GLOBAL_ARGS="--raft --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum"
 echo "[*] Starting geth"
-PRIVATE_CONFIG=tm.conf nohup geth --datadir qdata $GLOBAL_ARGS --rpccorsdomain "*" --rpcport 22000 --port 21000 --raftport 23000 --unlock 0 --password passwords.txt 2>>qdata/logs/geth.log &
+PRIVATE_CONFIG=raft/tm.ipc nohup geth --datadir qdata $GLOBAL_ARGS --rpccorsdomain "*" --rpcport 22000 --port 21000 --raftport 23000 --unlock 0 --password passwords.txt 2>>qdata/logs/geth.log &
