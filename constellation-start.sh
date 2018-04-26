e42a35d7a25467581f9986490b51171d16f17648
Himanshu-Pandey <38459237+Himanshu-Pandey@users.noreply.github.com>
2018-04-26 10:48:15 +0530

    Update constellation-start.sh

--- constellation-start.sh
+++ constellation-start.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
 set -u
 set -e
 
 echo "[*] Starting Constellation node"
-nohup constellation-node tm.conf 2>> qdata/logs/constellation.log &
+ARGS="--url=$(hostname -I | cut -f1 -d " ") --port=9000 --othernodes=[$1] --socket=qdata/tm.ipc  --publickeys=[raft/constellation.pub]  --privatekeys=raft/constellation.key  storage=qdata/constellation --verbosity=3"
+nohup constellation-node $ARGS  2>> qdata/logs/constellation.log &
