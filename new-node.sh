f48c215ea1936793d460a39a57ae9d12d1b76dc9
Himanshu-Pandey <38459237+Himanshu-Pandey@users.noreply.github.com>
2018-04-28 17:56:18 +0530

    Update new-node.sh

--- new-node.sh
+++ new-node.sh
@@ -5,36 +5,41 @@ echo "[*] Cleaning up temporary data directories"
 rm -rf raft
 mkdir raft
 
-echo "[*] Generating enode address"
+echo "[*] Generating geth node config"
 nohup geth --datadir raft 2>> raft/setup.log &
 sleep 3
 echo "[\"$(cat raft/setup.log | grep -oEi '(enode.*@)'|head -1)$(hostname -I | cut -f2 -d " "):21000?discport=0&raftport=23000\"]" >> raft/static-nodes.json
+
+# echo "[*] Creating default ethereum account"
+# geth --datadir raft --password passwords.txt account new
 
 echo "[*] Stopping geth"
 killall geth
 
 echo "[*] Generating constellation key pair"
-cd raft
-echo "" | constellation-node --generatekeys=constellation
-
-cd ..
+echo "" | constellation-node --generatekeys=raft/constellation
 
 echo "[*] Cleaning up temporary data directories"
 rm -rf qdata
 mkdir -p qdata/logs
 
-echo "[*] Making new account"
+echo "[*] Configuring node"
 mkdir -p qdata/{keystore,geth}
 cp raft/static-nodes.json qdata
 
 geth --datadir qdata --password passwords.txt account new
 cp raft/geth/nodekey qdata/geth/nodekey
-
-echo "[*] Initialising geth"
 geth --datadir qdata init genesis.json
 
 echo "[*] Starting Constellation node"
-ARGS="--url=http://$(hostname -I | cut -f2 -d " "):9000/ --port=9000 --workdir=raft --othernodes=[$1:9000/] --socket=tm.ipc  --publickeys=constellation.pub --privatekeys=constellation.key --verbosity=3"
+
+if [[ $1 ]]
+then
+    ARGS="--url=http://$(hostname -I | cut -f2 -d " "):9000/ --port=9000 --othernodes=[$1:9000/] --socket=qdata/tm.ipc  --publickeys=raft/constellation.pub --privatekeys=raft/constellation.key --verbosity=3"
+else
+    ARGS="--url=http://$(hostname -I | cut -f2 -d " "):9000/ --port=9000 --socket=qdata/tm.ipc  --publickeys=raft/constellation.pub --privatekeys=raft/constellation.key --verbosity=3"
+fi
+
 nohup constellation-node $ARGS  2>> qdata/logs/constellation.log &
 
 if [[ $1 ]]
@@ -51,5 +56,6 @@ then
 fi
 
 GLOBAL_ARGS="--raft --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum"
-echo "[*] Starting geth"
-PRIVATE_CONFIG=raft/tm.ipc nohup geth --datadir qdata $GLOBAL_ARGS --rpccorsdomain "*" --rpcport 22000 --port 21000 --raftport 23000 --unlock 0 --password passwords.txt 2>>qdata/logs/geth.log &
+
+echo "[*] Starting geth node"
+nohup geth --datadir qdata $GLOBAL_ARGS --rpccorsdomain "*" --rpcport 22000 --port 21000 --raftport 23000 --unlock 0 --raftjoinexisting 2 --password passwords.txt 2>>qdata/logs/geth.log &
