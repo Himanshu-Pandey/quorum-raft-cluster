90cf704d98023fd7a8563d3fae22c9722dc7c22c
Himanshu-Pandey <38459237+Himanshu-Pandey@users.noreply.github.com>
2018-04-28 17:58:59 +0530

    Update new-node.sh

--- new-node.sh
+++ new-node.sh
@@ -58,4 +58,4 @@ fi
 GLOBAL_ARGS="--raft --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum"
 
 echo "[*] Starting geth node"
-nohup geth --datadir qdata $GLOBAL_ARGS --rpccorsdomain "*" --rpcport 22000 --port 21000 --raftport 23000 --unlock 0 --raftjoinexisting 2 --password passwords.txt 2>>qdata/logs/geth.log &
+nohup geth --datadir qdata $GLOBAL_ARGS --rpccorsdomain "*" --rpcport 22000 --port 21000 --raftport 23000 --unlock 0 --password passwords.txt 2>>qdata/logs/geth.log &
